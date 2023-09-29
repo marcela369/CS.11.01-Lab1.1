f6d75668af7a65fc6103b1b344ae17623f5ab366
marcela <marcelac2026@student.cis.hk.com>
2023-09-29 12:00:52 +0800

    lab1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.*;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("Enter your age:");
+        Scanner scan =  new Scanner(System.in);
+        int age = scan.nextInt();
+
+        int newAge =  ( 7 + ( age / 2 ) );
+        System.out.println(age+"-year olds should date somebody who is at least " + newAge +" years old");
     }
 }
